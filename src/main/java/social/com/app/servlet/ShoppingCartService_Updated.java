d7812f5d01cabf80ca8d84a376b89b01e43da1f9
Bizlem-Dev <abhishek.tayshete@bizlem.com>
2019-05-25 16:52:17 +0530

    commit by Tejal

--- src/main/java/social/com/app/servlet/ShoppingCartService_Updated.java
+++ src/main/java/social/com/app/servlet/ShoppingCartService_Updated.java
@@ -86,20 +86,20 @@ protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse 
 	try {
 		out = response.getWriter();
 		session = repo.login(new SimpleCredentials(CrRuleConstValue.StringConstant.ADMIN.value(),
-				CrRuleConstValue.StringConstant.ADMIN.value().toCharArray()));
+		CrRuleConstValue.StringConstant.ADMIN.value().toCharArray()));
 		content = session.getRootNode().getNode(CrRuleConstValue.StringConstant.CONTENT.value());
 		if (!content.hasNode(CrRuleConstValue.StringConstant.USER.value())) {
-			USERNode = content.addNode(CrRuleConstValue.StringConstant.USER.value());
+		USERNode = content.addNode(CrRuleConstValue.StringConstant.USER.value());
 		} else {
-			USERNode = content.getNode(CrRuleConstValue.StringConstant.USER.value());
+		USERNode = content.getNode(CrRuleConstValue.StringConstant.USER.value());
 		}
-		out.println("USERNode  " + USERNode);
+		out.println("USERNode " + USERNode);
 		if (!content.hasNode(CrRuleConstValue.StringConstant.ALLSERVICES.value())) {
-			servicenode2 = content.addNode(CrRuleConstValue.StringConstant.ALLSERVICES.value());
+		servicenode2 = content.addNode(CrRuleConstValue.StringConstant.ALLSERVICES.value());
 		} else {
-			servicenode2 = content.getNode(CrRuleConstValue.StringConstant.ALLSERVICES.value());
+		servicenode2 = content.getNode(CrRuleConstValue.StringConstant.ALLSERVICES.value());
 		}
-		out.println("servicenode2  " + servicenode2);
+		out.println("servicenode2 " + servicenode2);
 
 		// static nodes
 		// end------------------------------------------------------------------------
@@ -107,118 +107,138 @@ protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse 
 		bufferedReaderCampaign = request.getReader();
 		String line = null;
 		while ((line = bufferedReaderCampaign.readLine()) != null) {
-			builder.append(line + "\n");
+		builder.append(line + "\n");
 		}
 		fulljsonobject = new JSONObject(builder.toString());
-		out.println("fulljsonobject  " + fulljsonobject);
+		out.println("fulljsonobject " + fulljsonobject);
 
 		// userId --mailId,serviceId --800,
 		username = fulljsonobject.getString("userId").replace("@", "_");
 		serviceid = fulljsonobject.getString("serviceId");
 		productcode = fulljsonobject.getString("productCode");
 		fullloaddata = fulljsonobject.getJSONArray("fullloaddata");
-		out.println("username  " + username);
-		out.println("serviceid  " + serviceid);
-		out.println("fullloaddata  " + fullloaddata);
+		out.println("username " + username);
+		out.println("serviceid " + serviceid);
+		out.println("fullloaddata " + fullloaddata);
 
 		if (!USERNode.hasNode(username)) {
-			AdminusernameNode = USERNode.addNode(username);
+		AdminusernameNode = USERNode.addNode(username);
 		} else {
-			AdminusernameNode = USERNode.getNode(username);
+		AdminusernameNode = USERNode.getNode(username);
 		}
-		out.println("AdminusernameNode  " + AdminusernameNode);
+		out.println("AdminusernameNode " + AdminusernameNode);
 
 		if (!AdminusernameNode.hasNode(CrRuleConstValue.StringConstant.SERVICES.value())) {
-			servicenode1 = AdminusernameNode.addNode(CrRuleConstValue.StringConstant.SERVICES.value());
+		servicenode1 = AdminusernameNode.addNode(CrRuleConstValue.StringConstant.SERVICES.value());
 		} else {
-			servicenode1 = AdminusernameNode.getNode(CrRuleConstValue.StringConstant.SERVICES.value());
+		servicenode1 = AdminusernameNode.getNode(CrRuleConstValue.StringConstant.SERVICES.value());
 		}
-		out.println("servicenode1  " + servicenode1);
+		out.println("servicenode1 " + servicenode1);
 
 		if (!servicenode1.hasNode(productcode)) {
-			productcodenode = servicenode1.addNode(productcode);
+		productcodenode = servicenode1.addNode(productcode);
 		} else {
-			productcodenode = servicenode1.getNode(productcode);
+		productcodenode = servicenode1.getNode(productcode);
 		}
-		out.println("productcodenode  " + productcodenode);
+		out.println("productcodenode " + productcodenode);
 
 		if (!productcodenode.hasNode(serviceid)) {
-			serviceidnode1 = productcodenode.addNode(serviceid);
+		serviceidnode1 = productcodenode.addNode(serviceid);
 		} else {
-			serviceidnode1 = productcodenode.getNode(serviceid);
+		serviceidnode1 = productcodenode.getNode(serviceid);
 		}
-		out.println("serviceidnode1  " + serviceidnode1);
+		out.println("serviceidnode1 " + serviceidnode1);
 
 		// ...........................................................................
 		// admin 2nd part add serviceid node in servicenode2
 
 		if (!servicenode2.hasNode(serviceid)) {
-			serviceidnode2 = servicenode2.addNode(serviceid);
-			serviceidnode2.setProperty("producttype", productcode);
+		serviceidnode2 = servicenode2.addNode(serviceid);
+		serviceidnode2.setProperty("producttype", productcode);
 		} else {
-			serviceidnode2 = servicenode2.getNode(serviceid);
-			serviceidnode2.setProperty("producttype", productcode);
+		serviceidnode2 = servicenode2.getNode(serviceid);
+		serviceidnode2.setProperty("producttype", productcode);
 		}
-		out.println("serviceidnode2  " + serviceidnode2);
+		out.println("serviceidnode2 " + serviceidnode2);
 
 		// admin node configuration
 		// end----------------------------------------------------------------------------------
 		// add for loop to save fullload users data
 		for (int i = 0; i < fullloaddata.length(); i++) {
-			JSONObject subobj = fullloaddata.getJSONObject(i);
-			String subuser = subobj.getString("user").replace("@", "_");
-			;
-			String role = subobj.getString("role");
-			String group = subobj.getString("group");
-			if (!USERNode.hasNode(username)) {
-				// add user in bizlem call httpurl connection
-				sendGet("http://prod.bizlem.io:8082/portal/process/shoppingcart/ShoppingCartUserCheckService?userId="
-						+ subobj.getString("user"));
+		JSONObject subobj = fullloaddata.getJSONObject(i);
+		out.println("subobj "+subobj);
+		String subuser = subobj.getString("user").replace("@", "_");
+		out.println("subuser "+subuser);
+		String role = subobj.getString("role");
+		out.println("role "+role);
+		String group = subobj.getString("group");
+		out.println("group "+group);
 
-				subusernode = USERNode.addNode(subuser);
-			} else {
-				subusernode = USERNode.getNode(subuser);
-			}
-			if (!subusernode.hasNode(CrRuleConstValue.StringConstant.SERVICES.value())) {
-				servicenode1 = subusernode.addNode(CrRuleConstValue.StringConstant.SERVICES.value());
-			} else {
-				servicenode1 = subusernode.getNode(CrRuleConstValue.StringConstant.SERVICES.value());
-			}
-			if (!servicenode1.hasNode(productcode)) {
-				productcodenode = servicenode1.addNode(productcode);
-			} else {
-				productcodenode = servicenode1.getNode(productcode);
-			}
-			if (!productcodenode.hasNode(serviceid)) {
-				serviceidnode1 = productcodenode.addNode(serviceid);
-			} else {
-				serviceidnode1 = productcodenode.getNode(serviceid);
-			}
-			if (!serviceidnode1.hasNode(group)) {
-				groupnode = serviceidnode1.addNode(serviceid);
-			} else {
-				groupnode = serviceidnode1.getNode(serviceid);
-			}
-			// ...........................................................................
-			// applicatio 2nd part
-			if (!serviceidnode2.hasNode(group)) {
-				groupnode = serviceidnode1.addNode(serviceid);
-			} else {
-				groupnode = serviceidnode1.getNode(serviceid);
-			}
 
-			if (!groupnode.hasNode(CrRuleConstValue.StringConstant.USERS.value())) {
-				usersnode = groupnode.addNode(CrRuleConstValue.StringConstant.USERS.value());
-			} else {
-				usersnode = groupnode.getNode(CrRuleConstValue.StringConstant.USERS.value());
-			}
-			if (!usersnode.hasNode(subuser)) {
-				subusersnode = usersnode.addNode(subuser);
-				subusersnode.setProperty("role", role);
-			} else {
-				subusersnode = usersnode.getNode(subuser);
-				subusersnode.setProperty("role", role);
-			}
+
+		System.out.println("subuser "+ subuser+" role "+role+" group "+group);
+		if (!USERNode.hasNode(subuser)) {
+		// add user in bizlem call httpurl connection
+		String result=	sendGet("http://prod.bizlem.io:8082/portal/process/shoppingcart/ShoppingCartUserCheckService?userId="
+		+ subobj.getString("user"));
+		System.out.println("result "+result);
+
+		subusernode = USERNode.addNode(subuser);
+		} else {
+		subusernode = USERNode.getNode(subuser);
+		}
+
+		out.println("subusernode "+subusernode);
+		if (!subusernode.hasNode(CrRuleConstValue.StringConstant.SERVICES.value())) {
+		servicenode1 = subusernode.addNode(CrRuleConstValue.StringConstant.SERVICES.value());
+		} else {
+		servicenode1 = subusernode.getNode(CrRuleConstValue.StringConstant.SERVICES.value());
+		}
+		out.println("servicenode1 "+servicenode1);
+
+		if (!servicenode1.hasNode(productcode)) {
+		productcodenode = servicenode1.addNode(productcode);
+		} else {
+		productcodenode = servicenode1.getNode(productcode);
+		}
+		out.println("productcodenode "+productcodenode);
+
+		if (!productcodenode.hasNode(serviceid)) {
+		serviceidnode1 = productcodenode.addNode(serviceid);
+		} else {
+		serviceidnode1 = productcodenode.getNode(serviceid);
+		}
+		out.println("serviceidnode1 "+serviceidnode1);
+
+		if (!serviceidnode1.hasNode(group)) {
+		groupnode = serviceidnode1.addNode(group);
+		} else {
+		groupnode = serviceidnode1.getNode(group);
+		}
+		out.println("groupnode "+groupnode);
+
+		// ...........................................................................
+		// applicatio 2nd part
+		if (!serviceidnode2.hasNode(group)) {
+		groupnode = serviceidnode2.addNode(group);
+		} else {
+		groupnode = serviceidnode2.getNode(group);
+		}
+		out.println("groupnode "+groupnode);
+
+
+		if (!groupnode.hasNode(CrRuleConstValue.StringConstant.USERS.value())) {
+		usersnode = groupnode.addNode(CrRuleConstValue.StringConstant.USERS.value());
+		} else {
+		usersnode = groupnode.getNode(CrRuleConstValue.StringConstant.USERS.value());
+		}
+		if (!usersnode.hasNode(subuser)) {
+		subusersnode = usersnode.addNode(subuser);
+		subusersnode.setProperty("role", role);
+		} else {
+		subusersnode = usersnode.getNode(subuser);
+		subusersnode.setProperty("role", role);
+		}
 
 		}
 
@@ -235,9 +255,10 @@ protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse 
 		// serivces/doctiger/789/G1
 
 		session.save();
-	} catch (Exception e) {
-		// out.println(e.getMessage());
-	}
+		} catch (Exception e) {
+		out.println(e.getMessage());
+		e.printStackTrace();
+		}
 	
 }
 
